30f4cec12f52c8378f99b89b1230adc9e227887d
OldScripter <Oldscripter@gmail.com>
2021-06-16 19:59:12 +0300

    GetRabbitWays function is added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+int GetRabbitWays(int stair_num)
+{
+    if (stair_num == 0) return 1; // He will just stay still
+    if (stair_num == 1) return 1;
+    if (stair_num == 2) return 2;
+
+    return  GetRabbitWays(stair_num - 1) +
+            GetRabbitWays(stair_num - 2) +
+            GetRabbitWays(stair_num - 3);
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int stair_number = 3;
+    std::cout << "Rabbit is trying to reach the stair number " << stair_number << ".\n";
+    std::cout << "He can do it in " << GetRabbitWays(stair_number) << " different ways.\n";
     return 0;
 }
